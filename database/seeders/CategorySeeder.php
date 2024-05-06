d506615df097a98ee7da84ab9bca6d120f5da42d
ubukata-shota <shota.ubukata.4197181@gmail.com>
2024-05-06 16:25:19 +0900

    カテゴリーシーダー

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -16,10 +16,37 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        DB::table('categories')->insert([
+        DB::table('cuisines')->insert([
+            [
+                'name' => '中華',
+                'created_at' => '2024-04-03 12:01:38',
+                'updated_at' => '2024-04-03 12:01:38',
+            ],
+            [
+                'name' => '和食',
+                'created_at' => '2024-04-03 12:02:28',
+                'updated_at' => '2024-04-03 12:02:28',
+            ],
+            [
+                'name' => '洋食',
+                'created_at' => '2024-04-03 12:04:39',
+                'updated_at' => '2024-04-03 12:04:39',
+            ],
+            [
+                'name' => 'イタリアン',
+                'created_at' => '2024-04-05 10:37:32',
+                'updated_at' => '2024-04-05 10:37:32',
+            ],
+            [
+                'name' => 'アジア・エスニック',
+                'created_at' => '2024-04-06 13:38:40',
+                'updated_at' => '2024-04-06 13:38:40',
+            ],
+            [
                 'name' => 'その他',
-                'created_at' => new DateTime(),
-                'updated_at' => new DateTime(),
-         ]);
+                'created_at' => '2024-04-06 13:43:00',
+                'updated_at' => '2024-04-06 13:43:00',
+            ],
+        ]);
     }
 }
