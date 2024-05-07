9dfcc083f853e2bd74966d3f9c9eb3d9aa718c96
ubukata-shota <shota.ubukata.4197181@gmail.com>
2024-05-07 21:01:14 +0900

    header

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,8 +1,9 @@
+
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('プロフィール') }}
-        </h2>
+        <div class="header profile">
+        　<p>プロフィール</p>
+        </div>
     </x-slot>
 
     <div class="py-12">
