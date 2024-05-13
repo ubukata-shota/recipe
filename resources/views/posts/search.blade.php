5e1b1af4b7845856b1f50c9a5d878e6e70e6d6e5
ubukata-shota <shota.ubukata.4197181@gmail.com>
2024-05-13 18:49:49 +0900

    CSS

--- resources/views/posts/search.blade.php
+++ resources/views/posts/search.blade.php
@@ -42,7 +42,7 @@
                 <div class="image">
                     <a href="/posts/{{ $post->id }}">
                         @if($post->image == null)
-                            <img src="{{ asset( "storage//jsI3pr0bNdUS1HypyALxx1uM7hHi2Sj6I6NltBUs.jpg" ) }}" alt="Post Image">
+                            <img src="{{ asset( "/jsI3pr0bNdUS1HypyALxx1uM7hHi2Sj6I6NltBUs.jpg" ) }}" alt="Post Image">
                         @else
                             <img src="{{ asset( $post->image) }}" alt="Post Image">
                         @endif
@@ -52,7 +52,7 @@
             </ul>
         @else
             <p class="result">検索結果はありません。</p>
-            <img class="no_result" src="{{ "storage/WStqSD6FwTUe2xCssDUoiXUmdjA94mHUMGDP3LdR.png" }}" alt="Post Image">
+            <img class="no_result" src="{{ "/WStqSD6FwTUe2xCssDUoiXUmdjA94mHUMGDP3LdR.png" }}" alt="Post Image">
         @endif
 
 <!-- ページネーションリンク -->
